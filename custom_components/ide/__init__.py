7ccffc1afd0307ea88a3cd3739933a9755a96d13
da-go-ga <103254590+da-go-ga@users.noreply.github.com>
2022-04-08 12:45:47 +0200

    Update __init__.py
    Confir ID to add a ID Contract for change where an account has more than one
    In that URL with you account logged in I-DE, you can view the contracts
    https://www.i-de.es/consumidores/rest/cto/listaCtos/

--- custom_components/ide/__init__.py
+++ custom_components/ide/__init__.py
@@ -2,7 +2,7 @@ import logging
 import voluptuous as vol
 from homeassistant.helpers import config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import CONF_USERNAME, CONF_PASSWORD
+from homeassistant.const import CONF_USERNAME, CONF_PASSWORD, CONF_ID
 from homeassistant.helpers.typing import HomeAssistantType
 
 __VERSION__ = "0.2.0"
@@ -15,12 +15,13 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Optional(CONF_USERNAME): cv.string,
         vol.Optional(CONF_PASSWORD): cv.string,
+        vol.Optional(CONF_ID): cv.string,
     }
 )
 
 
 async def async_setup(hass: HomeAssistantType, config):
-    has_credentials = CONF_USERNAME in config and CONF_PASSWORD in config
+    has_credentials = CONF_USERNAME in config and CONF_PASSWORD in config and CONF_ID in config
     if not has_credentials:
         _LOGGER.debug("No credentials provided")
     return True
